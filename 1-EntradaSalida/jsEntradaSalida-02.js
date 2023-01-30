902e488090e56c9e6aa95e87fb9182cfe8c97b8a
adrian herrera <adriherrera611@gmail.com>
2023-01-30 16:02:45 -0300

    clase 1

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -3,6 +3,19 @@ Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("ok");
+	// alert("ok");
+
+	// var edad;
+	let nombre;
+	let nombreCompleto; // Notacion lower camel case
+
+	//nombre = "adrian"; // Asignacion de string.
+nombre = prompt ("ingrese un nombre")
+
+	
+// Mostrar por alert
+	alert(nombre);
+
+
 }
 
